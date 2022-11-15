d43af052038ed27474c382de18e16fe1025bd33a
Eren Kafadar <ekafadar@f8ffc2583d87.uphs.upenn.edu>
2022-11-15 16:17:24 -0500

    env vars

--- pfn_jobs_loop.sh
+++ pfn_jobs_loop.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #$ -N pfn_loop
 #$ -o /cbica/projects/bgd-pfn/$JOB_NAME.log
 #$ -j y
@@ -6,8 +6,8 @@
 for i in {1..2}
 do
 for k in {1..2}
-export network = $i
-export vertex = $k
+export network=$i
+export vertex=$k
 qsub umx_on_vertex.sh -V
 sleep 30
 done
--- umx_on_vertex.sh
+++ umx_on_vertex.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #$ -N pfn_vertex_umx
 #$ -o /cbica/projects/bgd-pfn/pfn_vertex_output/$JOB_NAME_$JOB_ID.log
 #$ -j y
