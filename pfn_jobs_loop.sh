81a05a47ee2ca2551d4a58a53d21d6ed97f8af81
Eren Kafadar <ekafadar@f8ffc2583d87.uphs.upenn.edu>
2022-12-07 16:34:02 -0500

    R loop

--- pfn_jobs_loop.sh
+++ pfn_jobs_loop.sh
@@ -4,21 +4,27 @@
 #$ -j y
 #n_pfn = $1
 #n_vertex = $2 == 59412
-for n in {1..1}
+
+# for (( n=$1; n<=$2; n++))
+# do
+#   k_start = 1
+#   for  (( k=$3; k<=$4; k+=900))
+#   do
+#     k_end = $k
+#     qsub matlab_test.sh "$n_start" "$k_start" "$k_end"
+#     k_start = $k_end + 1
+#   done
+# done
+# file="/cbica/projects/bgd-pfn/vertex_columns/PFN"
+# file+="$n"
+# file+="V$k.csv"
+# while [ ! -f $file ]; # true if /your/file does not exist
+# do
+#   sleep 2;
+# done
+for ((n=$1; n<=$2; n++))
 do
-for k in {1..20}
-do
-qsub matlab_test.sh "$n" "$k"
-file="/cbica/projects/bgd-pfn/vertex_columns/PFN"
-file+="$n"
-file+="V$k.csv"
-while [ ! -f $file ]; # true if /your/file does not exist
-do
-  sleep 2;
-done
-sleep 1;
-qsub umx_on_vertex.sh "$n" "$k"
-sleep 30
-done
-sleep 60
+  k=$3
+  qsub umx_on_vertex.sh "$n" "$k"
+  sleep 2h;
 done
--- umx_on_vertex.sh
+++ umx_on_vertex.sh
@@ -13,9 +13,17 @@ conda activate umx-4.15
 home=/cbica/projects/bgd-pfn/pfn_umx/
 #cd $home
 network=$1
-vertex=$2
+vertex_lim=$2
 #vertex=$SGE_TASK_ID  #this is a special variable that is replaced with 1:n
 
 #matlab -r "call_matlab_fct($INDEX, '/cbica/projects/bgd-pfn/pfn1_vertex_pairs.csv');quit"
-matlab -r "get_vertex_col($network,$vertex);quit"
-Rscript  ${home}vertex_umx.R $network $vertex
+#matlab -r "get_vertex_col($network,$vertex);quit"
+for  ((k=1; k<=$vertex_lim; k++))
+do
+Rscript  ${home}vertex_umx.R $network $k
+if ($(($k%1000))==0);
+  then
+    echo "n $n k $k"
+    sleep 1m
+fi
+done
