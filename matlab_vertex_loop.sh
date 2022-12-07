8604fd60f53ab7b7246d3cffd0169e2d6ed32084
Eren Kafadar <ekafadar@f8ffc2583d87.uphs.upenn.edu>
2022-12-07 13:49:45 -0500

    m

--- matlab_vertex_loop.sh
+++ matlab_vertex_loop.sh
@@ -7,18 +7,18 @@
 
 for ((n=$1; n<=$2; n++))
 do
-  $k_start=1
+  $k_start=(1)
   for  ((k=$3; k<=$4; k+=1000))
   do
     let k_end=$k
-    if (($k_end>59412));
+    if (($k_end>(59412)));
     then
-      let k_end=59412
-      echo $n $k_start $k_end $k
+      let k_end=(59412)
+      echo "n $n k start $k_start k end $k_end k $k"
       qsub matlab_test.sh "$n" "$k_start" "$k_end"
       break
     fi
     qsub matlab_test.sh "$n" "$k_start" "$k_end"
-    let k_start=$k_end++
+    let k_start=($k_end++)
   done
 done
